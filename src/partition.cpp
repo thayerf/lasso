2878f94cccf6d879a2fc452743248e410c689e8b
thayerf <thayerfisher@oxy.edu>
2017-11-06 00:00:21 -0800

    reorganize class structure.

--- src/partition.cpp
+++ src/partition.cpp
@@ -1,20 +1,8 @@
 #include <armadillo>
 #include <iostream>
-#include "optimize.cpp"
+#include "optimize.hpp"
+#include "partition.hpp"
 using namespace arma;
-class Partition{
- private:
- int k_counter_;
- public:
-  mat x_;
-  colvec y_;
-  colvec b_;
-  colvec lambda_;
-  mat results_;
-  double t_;
-  int k_;
-
-
   /// @brief Constructor for partition object with no prespecified lambda
   /// value.
   /// @param[in] x
@@ -27,38 +15,30 @@ class Partition{
   /// Step size.
   /// @param[in] k
   /// Number of groups in partition.
-  Partition(mat x, colvec y, colvec beta, double t, int k) : x_(x), y_(y), b_(beta), t_(t), k_(k) {
+  Partition::Partition(mat x, colvec y, int k) : x_(x), y_(y){
     double lmax = CalcLmax(x_, y_);
-    lambda_ = linspace<vec>(lmax, lmax / 20, 100);
-    k_counter_=0;
-    results_=mat(lambda_.size(),k);
+    colvec lambda= linspace<vec>(lmax, lmax / 20, 100);
+    opt.SetParams(lambda,"l2",x_.n_cols);
+    partition_counter_=0;
+    partition_size_= x_.n_rows/k;
+    results_=mat(100,k);
   }
 
-  void IteratePartition(){
-    mat x_k1=x_.rows(0,k_counter_*x_.n_rows/k_ );
-    mat x_k2=x_.rows((k_counter_+1)*x_.n_rows/k_ -1,x_.n_rows-1);
-    mat x_k=join_cols(x_k1,x_k2);
-    colvec y_k1=y_.subvec(0,k_counter_*x_.n_rows/k_);
-    colvec y_k2=y_.subvec((k_counter_+1)*x_.n_rows/k_ -1 ,x_.n_rows-1);
-    colvec y_k=join_cols<mat>(y_k1,y_k2);
-    colvec b_k(1000, 1);
-    b_k.fill(0);
-    for(int i=0;i<lambda_.size();i++){
-    b_k=ggd(x_k,y_k,b_k,lambda_(i),t_);
-    if(k_counter_==k_-1){
-    x_k1=x_.rows(0,x_.n_rows/k_-1);
-    y_k1=y_.subvec(0,x_.n_rows/k_-1);
-    }
-    else{
-      x_k1=x_.rows(k_counter_*x_.n_rows/k_, (k_counter_+1)*x_.n_rows/k_ -1);
-      y_k1=y_.subvec(k_counter_*x_.n_rows/k_, (k_counter_+1)*x_.n_rows/k_ -1);
-    }
-    results_(i,k_counter_)=pred_error(x_k1,b_k,y_k1);
-    }
-    k_counter_++;
+  void Partition::IteratePartition(){
+    mat test_x= x_.rows(partition_counter_*partition_size_,(partition_counter_+1)*(partition_size_)-1);
+    mat train_x= x_;
+    train_x.shed_rows(partition_counter_*partition_size_,(partition_counter_+1)*(partition_size_)-1);
+
+    colvec test_y=y_.subvec(partition_counter_*partition_size_,(partition_counter_+1)*(partition_size_)-1);
+    colvec train_y= y_;
+    train_y.shed_rows(partition_counter_*partition_size_,(partition_counter_+1)*(partition_size_)-1);
+
+    results_.col(partition_counter_)=opt.partition_step(train_x,train_y,test_x,test_y);
+    cout<<"Partition "<<partition_counter_<<" complete."<<endl;
+    partition_counter_++;
   }
-  colvec PartitionCycle(){
-    while(k_counter_<k_){
+  colvec Partition::PartitionCycle(){
+    while(partition_counter_<x_.n_rows/partition_size_){
       IteratePartition();
     }
     int maxindex=0;
@@ -71,37 +51,11 @@ class Partition{
         maxindex=i;
       }
     }
-    cout<<"Best lambda value is: " << lambda_(maxindex)<< ". With average prediction error of "<< best<<"."<<endl;
-    return ggd(x_,y_,b_,lambda_(maxindex),t_);
+    cout<<"Best lambda value is: " << opt.lambda_(maxindex)<< ". With MSE of "<< best<<"."<<endl;
+    cout<<results_<<endl;
+    return opt.l2ggd(x_,y_,opt.b_,opt.lambda_(maxindex));
   }
-    double soft_threshold(double x, double lambda) {
-    if (x > lambda)
-      return x - lambda;
-    else if (x < -lambda)
-      return x + lambda;
-    else
-      return 0;
-  }
-  double pred_error(mat x, colvec b, colvec y){
-    return norm(x*b-y,2);
-  }
-  colvec ggd(mat x, colvec y, colvec b, double lambda, double t) {
-    /// We want b= b+(t/N)x'(y-xb)
-    colvec update;
-    update = b+(t)*(x.t()/x.n_rows)*(y-(x * b));
-    for (unsigned int i = 0; i < update.size(); i++) {
-      update(i) = soft_threshold(update(i), t * lambda);
-    }
-    if (norm(update - b) < 1e-4) {
-      b = update;
-      return b;
-    } else {
-      b = update;
-      b = ggd(x,y,b,lambda,t);
-      return b;
-    }
-  }
-double CalcLmax(mat x, colvec y){
+double Partition::CalcLmax(mat x, colvec y){
   double lmax=0;
   double l;
   for(int i=0;i<x.n_cols;i++){
@@ -110,6 +64,6 @@ double CalcLmax(mat x, colvec y){
   }
   return lmax;
 }
-};
+
 
 
--- src/test.cpp
+++ src/test.cpp
@@ -3,7 +3,7 @@
 #include <armadillo>
 #include <iostream>
 #include "catch.hpp"
-#include "partition.cpp"
+#include "partition.hpp"
 namespace arma {
 TEST_CASE("test", "[test]") {
   /// Initialize a column vector Y from our data.
@@ -12,17 +12,9 @@ TEST_CASE("test", "[test]") {
   /// Initialize a matrix X from our data.
   mat X;
   X.load("X.txt");
-  /// Choose arbitrary step size.
-  double t = .05;
-  /// Initial guess of 1 for all beta's
-  colvec beta(1000, 1);
-  beta.fill(0);
-  /// Initialize 1 (for now) lambda value.
-  colvec lambda(1, 1);
-  lambda.fill(.05);
   /// Create GGD object.
-  Partition test = Partition(X,Y,beta,t,2);
-  beta=test.PartitionCycle();
+  Partition test = Partition(X,Y,5);
+  colvec beta=test.PartitionCycle();
   cout<<beta(0)<<endl;
   cout<<beta(1)<<endl;
   cout<<beta(2)<<endl;
